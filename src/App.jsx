45f905242ec2471785a21cf26735b46d1da2bb9c
Ahmed Kemala <kemalaahmed@gmail.com>
2023-09-13 19:15:29 +0200

    feat: first zustand test -> counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import "./App.css";
+import { useCounterStore } from "./assets/utils/store/counter.store";
 
 function App() {
+  const count = useCounterStore((state) => state.count);
+
   return (
     <div>
-      <p>Hello World !</p>
+      <h1>First Zustand test</h1>
+      <span>{count}</span> <br />
+      <button onClick={useCounterStore((state) => state.inc)}>Increase</button>
+      <button onClick={useCounterStore((state) => state.dec)}>Decrease</button>
+      <button onClick={useCounterStore((state) => state.reset)}>Reset</button>
     </div>
   );
 }
